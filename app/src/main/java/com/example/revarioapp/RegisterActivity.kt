3a7fe1f01ec6b16c5bfb787cf7eef27a13b033ac
revario <revariosayiddinaalhabsy@gmail.com>
2025-02-05 22:36:28 +0700

    Sqlite dan register

--- app/src/main/java/com/example/revarioapp/RegisterActivity.kt
+++ app/src/main/java/com/example/revarioapp/RegisterActivity.kt
@@ -21,7 +21,9 @@ class RegisterActivity : AppCompatActivity() {
         val username = findViewById<EditText>(R.id.editTextUsername)
         val password = findViewById<EditText>(R.id.editTextPassword)
         val registerButton = findViewById<Button>(R.id.buttonRegister)
+        val backButton = findViewById<Button>(R.id.buttonRegister2)
 
+        // Set onClickListener untuk tombol Register
         registerButton.setOnClickListener {
             val name = fullName.text.toString()
             val user = username.text.toString()
@@ -40,8 +42,12 @@ class RegisterActivity : AppCompatActivity() {
                 showErrorDialog("Input Kosong", "Harap isi semua field!")
             }
         }
-    }
 
+        // Set onClickListener untuk tombol Back
+        backButton.setOnClickListener {
+            finish()  // Menutup RegisterActivity dan kembali ke LoginActivity
+        }
+    }
 
     private fun showErrorDialog(title: String, message: String) {
         AlertDialog.Builder(this)
@@ -51,3 +57,4 @@ class RegisterActivity : AppCompatActivity() {
             .show()
     }
 }
+
